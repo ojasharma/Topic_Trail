df7b23505475a146678836286f538d811e891573
Namita <namita3599@gmail.com>
2024-12-15 11:44:16 -0500

    Initial commit with basic Vite + React setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Welcome to Topic Trail</h1>
+        <p>Your one-stop solution for topic-based video search!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
