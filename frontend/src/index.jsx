5077297c38d449bd9bf0f9903d13758b55a841a0
Namita <namita3599@gmail.com>
2024-12-19 01:30:12 -0500

    changes to index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter , Route, Routes } from 'react-router-dom'; // Importing Router
+import { BrowserRouter } from 'react-router-dom'; // Importing Router
 import App from './App';
 import './index.css'; // Global styles
+import 'react-toastify/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
