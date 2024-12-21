9793cb791cb2853547f0c047b62a0f70e93e19a1
Namita <namita3599@gmail.com>
2024-12-21 01:41:36 -0500

    home page structure and css update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Login from './pages/Login/'
 import Signup from './pages/Signup/'
 import Home from './pages/Home/'
+import ClassDetails from "./pages/ClassDetails";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/home' element={<Home />} />
+      <Route path='/class/:id' element={<ClassDetails />} />
     </Routes>
   );
 }
