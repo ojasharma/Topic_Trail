0cf56fcd6dceea91d76d4d210d865a6a62eda2ce
Ojasvi Sharma <ojasvishamacs@gmail.com>
2024-12-27 18:58:22 +0530

    added footer jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Footer.module.css"; // Import the CSS module
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <p>
+          <a
+            href="https://github.com/dieselsharma"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/github-logo.png"
+              alt="GitHub"
+              className={styles.githubIcon}
+            />
+          </a>
+          Backend & UI/UX by Ojasvi
+        </p>
+
+        <p>
+          <a
+            href="https://github.com/namita3599"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/github-logo.png"
+              alt="GitHub"
+              className={styles.githubIcon}
+            />
+          </a>
+          Frontend by Namita
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
