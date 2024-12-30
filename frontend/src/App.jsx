97e9b2d00d9d0ff904feca0bee3b3acbbf1a7df7
Ojasvi Sharma <ojasvishamacs@gmail.com>
2024-12-30 16:27:57 +0530

    changed aapp/jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider } from "./components/ThemeContext"; // Make sure ThemeProvider is imported
 import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -7,57 +9,60 @@ import ClassDetails from "./pages/ClassDetails";
 import VideoDetails from "./pages/VideoDetails";
 
 function App() {
+  // The theme state and toggle logic will now be handled by the ThemeProvider.
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Navigate to="/login" />
-          </Layout>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <Layout>
-            <Login />
-          </Layout>
-        }
-      />
-      <Route
-        path="/signup"
-        element={
-          <Layout>
-            <Signup />
-          </Layout>
-        }
-      />
-      <Route
-        path="/home"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
-      <Route
-        path="/class/:id"
-        element={
-          <Layout>
-            <ClassDetails />
-          </Layout>
-        }
-      />
-      <Route
-        path="/video/:videoId"
-        element={
-          <Layout>
-            <VideoDetails />
-          </Layout>
-        }
-      />
-    </Routes>
+    <ThemeProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Navigate to="/login" />
+            </Layout>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Layout>
+              <Signup />
+            </Layout>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/class/:id"
+          element={
+            <Layout>
+              <ClassDetails />
+            </Layout>
+          }
+        />
+        <Route
+          path="/video/:videoId"
+          element={
+            <Layout>
+              <VideoDetails />
+            </Layout>
+          }
+        />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
